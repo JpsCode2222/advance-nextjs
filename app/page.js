3c8ed3f606b3875b41cc76fb5b52ae210124ff24
Jayad Pathan <jayadpathandev@gmail.com>
2024-11-30 19:28:20 +0530

    get users data using axios

--- app/page.js
+++ app/page.js
@@ -1,11 +1,38 @@
+'use client'
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 function page(){
+  const [user,setUser] = useState([]);
+  const getUsersData = async()=>{
+    let {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+    setUser(data);
+  }
+
+  let renderJs = <p className="text-black">Users not found !</p>
+  if(user.length > 0){
+    renderJs = user.map((user,id)=>{
+      return <li key={id}>{user.username}</li>
+    })
+  }
   return(
     <>
+    <div className="p-3">
     <h1>Home Page</h1>
     <Link href="/contact" className="text-blue-500"> go to contact page</Link>
+
+    <br/>
+    {/* getting user data */}
+    <button onClick={getUsersData} className="mt-3 px-4 py-2 bg-slate-300 rounded text-black">Get Data</button>
+    <div className="bg-slate-400 p-5 mt-5">
+      <ul>
+        {
+         renderJs
+        }
+      </ul>
+    </div>
+    </div>
     </>
   )
 }
