50482c2f7059ee27ce5436873d0a0a3342dede77
Jayad Pathan <jayadpathandev@gmail.com>
2024-11-30 18:09:27 +0530

    add routing using link

---
+++ app/contact/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1>Contact Page</h1>
+    <Link href="/" className='text-blue-500'>go to home page</Link>
+    </>
+  )
+}
+
+export default page
